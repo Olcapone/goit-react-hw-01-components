66fc9b72608b482bfe3cf615d8b37450109a5f48
OLgaPonomarenko <fblackberries@gmail.com>
2021-08-12 23:36:58 +0300

    bag

--- src/components/FriendListItem/FriendListItem.js
+++ src/components/FriendListItem/FriendListItem.js
@@ -5,7 +5,7 @@ import s from "./FriendListItem.module.css";
 export default function FriendListItem({ isOnline, avatar, name }) {
   return (
     <Fragment>
-      <span className={isOnline ? s.online : s.offline}></span>
+      <span className={isOnline ? s.online : s.offline} />
       <img className={s.avatar} src={avatar} alt="" width="48" />
       <p className={s.name}>{name}</p>
     </Fragment>
