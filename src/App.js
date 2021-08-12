0598c34041c0760c7ce0851feaf62ed9de471247
OLgaPonomarenko <fblackberries@gmail.com>
2021-08-12 05:46:12 +0300

    3 complete

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React, { Fragment } from "react";
 
 import "./App.css";
 import PaintProfile from "./components/PaintProfile/PaintProfile";
-import Statistic from "./components/Statistic/Statistic";
 import userData from "./user.json";
-import statisticalData from "./statisticsData.json";
+import Statistic from "./components/Statistic/Statistic";
+import statisticlData from "./statisticsData.json";
+import FriendList from "./components/FriendList/FriendList";
+import friendsData from "./friends.json";
 
 export default function App() {
   return (
     <Fragment>
       <PaintProfile {...userData} />
-      <Statistic options={statisticalData} />
+      <Statistic options={statisticlData} />
+      <FriendList friends={friendsData} />
     </Fragment>
   );
 }
---
+++ src/components/FriendListItem/FriendListItem.js
@@ -0,0 +1,21 @@
+import PropTypes from "prop-types";
+import React, { Fragment } from "react";
+import s from "./FriendListItem.module.css";
+
+export default function FriendListItem({ ...friends }) {
+  const { isOnline, avatar, name } = friends;
+
+  return (
+    <Fragment>
+      <span className={isOnline ? s.online : s.offline}></span>
+      <img className={s.avatar} src={avatar} alt="" width="48" />
+      <p className={s.name}>{name}</p>
+    </Fragment>
+  );
+}
+
+FriendListItem.propTypes = {
+  avatar: PropTypes.string,
+  name: PropTypes.string,
+  isOnline: PropTypes.bool,
+};
